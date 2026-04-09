518b971b235be12b8c4ee28cc29bcf2190f41ff2
NahueSanz <nahue.p.sanz@gmail.com>
2026-04-09 20:34:38 -0300

    asterisco a los headers

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-club-id',"*"],
 }));
 
 app.options('*', cors());
